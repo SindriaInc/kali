6081d1a9016dc157232b6ddf3eddf7307fdf6141
lucapitzoi <luca.pitzoi@sindria.org>
2021-08-02 17:08:22 +0200

    Fix sublister, Fix entrypoint

--- src/resources/bin/entrypoint.sh
+++ src/resources/bin/entrypoint.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
 # Fix permission
-chown -R hackerino:hackerino /var/www/app
+chown -R ${SINDRIA_USER}:${SINDRIA_USER} /var/www/app
 
 # Override timezone by env
-if [ "$TZ" != "" ] || [ "$TZ" != "Europe/Rome" ]; then
-    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+if [ "${TZ}" != "" ] || [ "${TZ}" != "Europe/Rome" ]; then
+    ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
 fi
 
 # Override host user uid by env
-if [ "$HOST_USER_UID" != "1000" ]; then
-    usermod -u $HOST_USER_UID hackerino && groupmod hackerino -g $HOST_USER_UID
-fi
-
-# Override default sindria user password by env
-if [ "$DEFAULT_SINDRIA_USER_PASSWORD" != "hackerino" ]; then
-    echo "${DEFAULT_SINDRIA_USER_PASSWORD}:hackerino" | chpasswd
+if [ "${HOST_USER_UID}" != "1000" ]; then
+    usermod -u ${HOST_USER_UID} ${SINDRIA_USER} && groupmod ${SINDRIA_USER} -g ${HOST_USER_UID}
 fi
---
+++ src/resources/scripts/sublister.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 /opt/Sublist3r/sublist3r.py
